93be8f049ccdb9fd016cb08afb76e1f56b49db87
talwinder50 <talwinderkaur50@gmail.com>
2017-11-13 19:11:23 -0500

    Test

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -2,7 +2,6 @@ package application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 
 @SpringBootApplication(scanBasePackages ={"configuration","controller"})
