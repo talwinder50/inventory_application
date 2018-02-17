747cd334705717b6841b819d9c1d8947a93487d5
talwinder50 <talwinderkaur50@gmail.com>
2018-02-17 16:42:47 -0500

    SSHJsch Implementation Update

--- inventory-app/src/main/java/services/SSHService.java
+++ inventory-app/src/main/java/services/SSHService.java
@@ -2,6 +2,7 @@ package services;
 
 
 
+import java.io.IOException;
 import java.util.Map;
 
 import com.jcraft.jsch.JSchException;
@@ -17,6 +18,7 @@ import services.model.RemoteServer;
 public interface SSHService {
 	
 	/**
+	 * @throws IOException 
 	 * TODO: Add more details about interface
 	 * @param filePath e.g. /apps/mindblowing/app.properties
 	 * @return Key and Values of configuration file
@@ -25,6 +27,6 @@ public interface SSHService {
  
   
 	*/
-	public Map<String, String> parseFile(RemoteServer remoteServer, RemoteFile remoteFile) throws JSchException;
+	public Map<String, String> parseFile(RemoteServer remoteServer, RemoteFile remoteFile) throws JSchException, IOException;
     
 }
--- inventory-app/src/main/java/services/SshJschImpl.java
+++ inventory-app/src/main/java/services/SshJschImpl.java
@@ -2,11 +2,15 @@ package services;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
+import java.util.Map.Entry;
 
 import org.hibernate.mapping.Set;
 import org.springframework.stereotype.Service;
@@ -18,55 +22,44 @@ import services.model.RemoteFile;
 import services.model.RemoteServer;
 
 /**
- * @author 
+ * @author
  * 
- * TODO: 
- * 1. This is SSHSerice Implementation
- * 2. It will call JschConnector for communication
- * 3. It will NOT have direct imports to Jsch library
- * 4. It is Spring Bean @Service
+ * 		TODO: 1. This is SSHSerice Implementation 2. It will call
+ *         JschConnector for communication 3. It will NOT have direct imports to
+ *         Jsch library 4. It is Spring Bean @Service
  *
  */
 @Service
 public class SshJschImpl implements SSHService {
-	
-	
 
 	@Override
 	public Map<String, String> parseFile(RemoteServer remoteServer, RemoteFile remoteFile) throws JSchException {
 		// TODO Auto-generated method stub
-		
-		JschConnector jsch = new JschConnector(remoteServer.getHost(),
-				                              remoteServer.getUser(),
-				                              remoteServer.getPort());
+
+		JschConnector jsch = new JschConnector(remoteServer.getHost(), remoteServer.getUser(), remoteServer.getPort());
 		try {
 			jsch.open();
-			jsch.executeCommand("scp -f "+remoteFile.getRemotefilepath());
-			Properties MyPropertyFile= new Properties();
-			FileInputStream ip = new FileInputStream(".properties file path");
-			MyPropertyFile.load(ip);
-			
-			Map<String, String> map=new Map<String, String>();
-			Set keys = (Set) MyPropertyFile.keySet();
-			
-			for(Object k:keys){
-			    String key=(String) k;
-			    String value=MyPropertyFile.getProperty(key);
+			jsch.executeCommand("scp -f " + remoteFile.getRemotefilepath());
+			Properties prop = new Properties();
+			Map<String, String> map = new HashMap<String, String>();
+			try {
+			FileInputStream inputStream = new FileInputStream(remoteFile.getRemotefilepath());
+				prop.load(inputStream);
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println("Some issue finding or loading file....!!! " + e.getMessage());
 
-			//    if(row.charAt(0)==value.charAt(0))// check row's first character and values first character are same.
-			        map.put(key, value);
-			  //  }
-			
+			}
+			for (final Entry<Object, Object> entry : prop.entrySet()) {
+				map.put((String) entry.getKey(), (String) entry.getValue());
+			}
+			return map;
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} finally {
+
 		}
-		
-		
 		return null;
 	}
-
-	 
-	
-
 }
