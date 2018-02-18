d296095e8d1ce25c608710d6a98425e302fe77b3
talwinder50 <talwinderkaur50@gmail.com>
2018-02-18 13:00:44 -0500

    remote file

---
+++ inventory-app/src/main/java/services/model/RemoteFile.java
@@ -0,0 +1,18 @@
+package services.model;
+
+public class RemoteFile {
+	
+	private String remotefilepath;
+	
+	
+
+	public String getRemotefilepath() {
+		return remotefilepath;
+	}
+
+	public void setRemotefilepath(String remotefilepath) {
+		this.remotefilepath = remotefilepath;
+	}
+	
+
+}
